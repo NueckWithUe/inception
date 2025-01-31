d74c23055a66ac67f37252252e99badec21e99da
Nils Nagel <nnagel@student.42heilbronn.de>
2025-01-31 14:16:43 +0100

    feat: started implementing nginx and changed some other stuff

--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', $DB_USER );
 define( 'DB_PASSWORD', $DB_PASS );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'nnagel.42.fr' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
