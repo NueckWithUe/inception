9ee8a95e46fe01fe48b8eb46ff68fa0311abc50f
Nils Nagel <nnagel@student.42heilbronn.de>
2025-02-04 14:53:21 +0100

    feat: added changes to nginx wordpress and mariadb to ensure the containers are connected

--- srcs/requirements/wordpress/tools/init_php.sh
+++ srcs/requirements/wordpress/tools/init_php.sh
@@ -31,7 +31,7 @@ define( 'DB_USER', $DB_USER );
 define( 'DB_PASSWORD', $DB_PASS );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
